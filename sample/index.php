12f8614f48dfe0d21b62b025c7292fcef576b53b
andrycs <andry.c.sutanto@gdplabs.id>
2016-06-17 16:52:01 +0700

    create sample oauth2 request

---
+++ sample/index.php
@@ -0,0 +1,47 @@
+<?php
+
+require __DIR__.'/../vendor/autoload.php';
+
+use GuzzleHttp\Client;
+use CommerceGuys\Guzzle\Oauth2\GrantType\RefreshToken;
+use CommerceGuys\Guzzle\Oauth2\GrantType\PasswordCredentials;
+use CommerceGuys\Guzzle\Oauth2\Oauth2Subscriber;
+
+$base_url = 'http://webstaging.kaskus.co.id';
+
+$oauth2Client = new Client(['base_url' => $base_url]);
+
+$config = [
+    'username' => 'recca',
+    'password' => 'kaskus',
+    'client_id' => '1',
+    'client_secret' => 'secret',
+    'scope' => 'thread.write,thread.read',
+    'token_url' => 'oauth/access-token',
+];
+
+$token = new PasswordCredentials($oauth2Client, $config);
+$refreshToken = new RefreshToken($oauth2Client, $config);
+
+$oauth2 = new Oauth2Subscriber($token, $refreshToken);
+
+$client = new Client([
+    'defaults' => [
+        'debug' => true,
+        'auth' => 'oauth2',
+        'subscribers' => [$oauth2],
+        'headers' => array(
+            'Return-Type' => 'text/json'
+        )
+    ],
+]);
+
+try {
+    $response = $client->get('http://webstaging.kaskus.co.id/api/oauth/v1/forum_thread/530d5df41e0bc3a8068b47a5');
+    var_dump($response->json());
+} catch (\Exception $e) {
+    var_dump($e);
+}
+
+ // var_dump($oauth2->getAccessToken());
+  // var_dump($oauth2->getRefreshToken());
